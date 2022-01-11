d4220ce14975af8d5a0e4377499072c74011cc1e
vgimenez0 <vgimenezc0@gmail.com>
2022-01-10 23:23:51 -0300

    2 commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class NavBar extends React.Component {
+    render() {
+        return {
+            <div class="topnav">
+                <p>Move it</p>
+                <a class="active" href="">Inicio</a>
+                <a href="">Mujeres</a>
+                <a href="#>Hombres</a>
+                <a href="#">Niños/as</a>
+            </div>
+        }
+}
